82a0ab59875756ffe50a4c706e9eb7d3e4dda597
ravindrasinghshah <ravindrasinghshah@gmail.com>
2020-09-18 07:31:26 -0400

    firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADffhq5qtDvRPkIaZ2sGpsv3ukVIGng2I",
+    authDomain: "covid-19-3a1ac.firebaseapp.com",
+    databaseURL: "https://covid-19-3a1ac.firebaseio.com",
+    projectId: "covid-19-3a1ac",
+    storageBucket: "covid-19-3a1ac.appspot.com",
+    messagingSenderId: "424916426150",
+    appId: "1:424916426150:web:d640df18f45ac45eefc367",
+    measurementId: "G-QKEGP835HN"
+  };
